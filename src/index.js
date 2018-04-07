efe94682997aa2bd08e361a474709eae3649a4f6
Thiago Santos <thia.mdossantos@gmail.com>
2018-04-07 13:29:13 -0300

    fix(Notification): export notification component

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export {default as Columns} from './components/columns/columns'
 export {default as Column} from './components/column/column'
+export {default as Notification} from './components/notification/notification'
